b5413c7b44fe9ac31239ead131981e6ec0afc093
Kevin Beck <kevinnb48@gmail.com>
2022-04-21 03:50:24 +0000

    Changed print to return in functions.

--- ds-prep-python-functions/main.py
+++ ds-prep-python-functions/main.py
@@ -7,23 +7,30 @@ def cube(base):
 cube(9)
 def absolute_value(a,b):
   if (a<b):
-    print(b-a)
+    return(b-a)
   else:
-    print(a-b)
+    return(a-b)
 print(absolute_value(12,9))
 def squared_value(a,b):
   if (a<b):
-    print((b-a)**2)
+    return((b-a)**2)
   else:
-    print((a-b)**2)
+    return((a-b)**2)
 print(squared_value(12,9))
 def hours_to_minutes(hours):
-  print(hours*60)
-hours_to_minutes(hours = 2)
+  return(hours*60)
+print(hours_to_minutes(hours = 2))
 def get_circumference(radius):
-  print(radius*2*3.14)
+  return(radius*2*3.14)
 print(get_circumference(radius=1))
 def linear_transform(x, slope, intercept):
-  print((slope*x)+intercept)
+  return((slope*x)+intercept)
 print(linear_transform(x=5, slope=2, intercept=3))
 print(linear_transform(intercept=2, x=6, slope=3))
+def standardize(x, x_center, scale_size):
+  if (x>x_center):
+    return((x-x_center)/scale_size)
+  else:
+    return((x_center-x)/scale_size)
+print(standardize(x=6, x_center=4, scale_size=2))
+print(standardize(scale_size=3, x=12, x_center=6))
